7611c67edda41f5b2656d54cca2dad1367cbd3b1
saranrajuk <saranrajusaran111@gmail.com>
2020-09-03 19:44:59 +0530

    done my code

---
+++ src/test/java/org/tcs/Hello.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class Hello {
+public static void main(String[] args) {
+	String s1="Java python DotNet Oracle";
+	String s2="e";
+int compareTo = s1.compareTo(s2);
+System.out.println(compareTo);
+boolean b = s1.endsWith(s2);
+System.out.println(b);
+}
+}
